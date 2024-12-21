cd7cbea785f35ddc30ff34ce41bc985a430cafae
Nikhil <nikhil.jmsyadav@gmail.com>
2024-12-21 19:37:26 +0530

    strong tag and b tag update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var count=99;
 var printlyrics=document.getElementById("lyrics");
 while (count>1) {
-    printlyrics.innerHTML += (count+ "</stong> bottles of beer on the wall, " +count+ " bottles of beer.<br>Take one down and pass it around, "+(count-1)+ " bottles of beer on the wall. <br> <br>");
+    printlyrics.innerHTML += ("<strong>"+count+ "</strong> bottles of beer on the wall, <strong>" +count+ "</strong> bottles of beer.<br>Take one down and pass it around, <strong>"+(count-1)+ "</strong> bottles of beer on the wall. <br> <br>");
     count--;
     if(count===1){
-        printlyrics.innerHTML += ("1 bottle of beer on the wall, 1 bottle of beer.<br>Take one down and pass it around, no more bottles of beer on the wall. <br>"); 
+        printlyrics.innerHTML += ("<b>1</b> bottle of beer on the wall,<b>1</b> bottle of beer.<br>Take one down and pass it around, no more bottles of beer on the wall. <br>"); 
         count--;
     }
 }
 if(count===0){
-    printlyrics.innerHTML += ("<br>No more bottles of beer on the wall, no more bottles of beer.<br>Go to the store and buy some more, 99 bottles of beer on the wall.");
+    printlyrics.innerHTML += ("<br>No more bottles of beer on the wall, no more bottles of beer.<br>Go to the store and buy some more, <b>99</b> bottles of beer on the wall.");
 }
